3ade5785f7606ae7bdf3a7963dd1f4f2c62a160a
durga thota <ramadurgaprasad@live.com>
2024-02-23 15:07:50 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿int age = 0;
+
+string name = string.Empty;
+
+Console.WriteLine("Please Enter Your Name:");
+
+name = Console.ReadLine();
+
+Console.WriteLine("Please Enter Your age:");
+
+age = Convert.ToInt32(Console.ReadLine());
+
+int ageEligibleToWork =62 - age;
+
+
+Console.WriteLine($"Your Name: {name}");
+
+Console.WriteLine($"Your working years remaining:{ageEligibleToWork}");
+
