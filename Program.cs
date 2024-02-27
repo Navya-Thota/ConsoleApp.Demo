4c94baa9e5b4b13855d7d781dc8a8cd834919c4c
durga thota <ramadurgaprasad@live.com>
2024-02-27 11:26:28 -0600

    added operators,Assaignment operators.

--- Program.cs
+++ Program.cs
@@ -3,12 +3,16 @@
 string firstName = string.Empty;
 
 string lastName = string.Empty;
+decimal salary =0;
+char gender = char.MinValue;
+bool working = false;
 
 Console.WriteLine("Please Enter Your first Name:");
 
+firstName = Console.ReadLine();
+
 Console.WriteLine("Please Enter Your last Name:");
 
-firstName = Console.ReadLine();
 lastName = Console.ReadLine();
 
 Console.WriteLine("Please Enter Your age:");
@@ -17,8 +21,22 @@ age = Convert.ToInt32(Console.ReadLine());
 
 int ageEligibleToWork =62 - age;
 
+Console.WriteLine("Enter your salary:");
+salary= Convert.ToDecimal(Console.ReadLine());
+
+Console.WriteLine("Enter your gender(M/F):");
+gender =Convert.ToChar(Console.ReadLine());
+
+Console.WriteLine("Are you Working?:");
+working=Convert.ToBoolean(Console.ReadLine());
+
 
 Console.WriteLine($"Full Name: {firstName} {lastName}");
 
 Console.WriteLine($"Your working years remaining:{ageEligibleToWork}");
+
+Console.WriteLine($"Your Salary is:{salary}");
+
+Console.WriteLine($"Your gender is:{gender}");
+Console.WriteLine($"Your working status{working}");
 
